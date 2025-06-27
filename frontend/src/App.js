4cbf610137c74b8a93e7ef9b2e6baf1ab2eed77e
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-27 20:59:02 -0300

    initial frontend app

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function App() {
+  return (
+    <div>
+      <h1>Rehearsal Scheduler</h1>
+      <p>Book your band and studio rehearsals online, get reminders, and track attendance.</p>
+      {/* Further frontend will be implemented here */}
+    </div>
+  );
+}
+export default App;
